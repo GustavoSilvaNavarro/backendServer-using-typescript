0f311a51bde723716382473d529060e4082001e0
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-08-31 09:06:44 -0300

    feat(morgan): morgan dependency was etup

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response, NextFunction, Application } from 'express';
+import morgan from 'morgan';
 
 // INITIALIZATIONS
 const app: Application = express();
 
 // SETTINGS
-app.set('port', 3000);
+app.set('port', process.env.PORT !== undefined ? process.env.PORT : 3000);
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
+app.use(morgan('dev'));
+
+app.get('/', (_req: Request, res: Response, _next: NextFunction) => {
   res.send('It is fucking working');
 });
 
